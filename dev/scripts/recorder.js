da3a73e3eb77af0b6690dc3be63f56c8b49c7818
Vinh Luu <vinhluu16@gmail.com>
2018-01-07 23:16:49 -0500

    additions

--- dev/scripts/recorder.js
+++ dev/scripts/recorder.js
@@ -4,20 +4,49 @@ export default class SongRecorder extends React.Component{
     constructor(){
         super();
         this.state = {
-            record: false
+            record: false,
+            songData: {}
         }
         this.recordSong = this.recordSong.bind(this);
+        this.stopRecord = this.stopRecord.bind(this);
     }
     //on window load we want to prompt the user for access to their microphone
     recordSong(e){
         e.preventDefault();
+        let recorder;
+        let chunks = [];
         navigator.mediaDevices.getUserMedia({
             audio: true
         })
-            this.setState({
-                record: false,
+            .then(function(stream){
+
+                recorder = new MediaRecorder(stream);
+                recorder.start();
+                console.log("clicked");
+                //as recording happens, we need to collect the audio data with ondataavailable
+                recorder.ondataavailable = function(e){
+                    chunks.push(e.data);
+                }
             })
+            this.setState({
+                record: true,
+                // songData: chunks
+            })
+            if(this.state.record === false){
+                // recorder.start();
+                this.setState({
+                    songData: chunks
+                })
+            }
         };
+
+    stopRecord(e){
+        e.preventDefault();
+    
+        this.setState({
+            record: false,
+        })
+    }
     
     render(){
         return(
